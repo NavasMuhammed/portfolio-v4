5a4f945000cdf3b0782adbfc614313bbb0204908
NavasMuhammed <navas3444a3@gmail.com>
2024-03-09 23:58:41 +0530

    feat(client): mobile responsive improvements

--- src/app/components/switchCompoent/switchComponent.tsx
+++ src/app/components/switchCompoent/switchComponent.tsx
@@ -10,8 +10,8 @@ const SwitchComponent = ({ Headder, title, actionMarkup, body }: SwitchComponent
     return (
         <div className="experience-education-container">
             <div className="flex flex-col items-center gap-4 justify-between">
-                <div className="text-4xl font-semibold">{Headder}</div>
-                <div className="text-sm">{title}</div>
+                <div className="text-2xl font-semibold text-center sm:text-4xl">{Headder}</div>
+                <div className="text-sm hidden sm:block">{title}</div>
                 {actionMarkup}
             </div>
             {body}
