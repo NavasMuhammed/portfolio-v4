c0db22f68341aacd258518967d2a837f7a392601
NavasMuhammed <navas3444a3@gmail.com>
2024-03-20 20:47:48 +0530

    feat(client): svg addition and css fix

--- src/app/components/projects/projectsSection.tsx
+++ src/app/components/projects/projectsSection.tsx
@@ -17,7 +17,7 @@ export const ProjectsSection = () => {
 
     return (
         <div className='flex mt-20 flex-wrap gap-8 sm:gap-0 '>
-            <div className='flex gap-6 flex-col flex-1 min-w-96  sm:sticky top-64 h-fit'>
+            <div className='flex gap-6 flex-col flex-1 min-w-96  sm:sticky top-56 h-fit'>
                 <span className='text-xl sm:text-2xl'>Projects I have worked on</span>
                 {/* <span className='hidden sm:block sm:text-5xl sm:leading-normal'>Does More Then Ever
                     Platform To Achieve Goal
@@ -25,7 +25,7 @@ export const ProjectsSection = () => {
                 <div className='pb-12 hidden sm:block'>
                     <Tools />
                 </div>
-                <ButtonProdiver link='https://www.canva.com/design/DAE4gakUnqs/VgJBFKzuM05-ySZIepAy4w/edit?utm_content=DAE4gakUnqs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton' title='Resume' className='hidden sm:flex m-0 w-max fancy-hover px-3.5 py-3 h-fit rounded-xl sm:mr-0 ' />
+                {/* <ButtonProdiver link='https://www.canva.com/design/DAE4gakUnqs/VgJBFKzuM05-ySZIepAy4w/edit?utm_content=DAE4gakUnqs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton' title='Resume' className='hidden sm:flex m-0 w-max fancy-hover px-3.5 py-3 h-fit rounded-xl sm:mr-0 ' /> */}
 
             </div>
             <div className='flex flex-wrap flex-1 gap-6'>
